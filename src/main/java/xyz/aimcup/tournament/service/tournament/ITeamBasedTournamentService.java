1e4a99ef3c72baad86ea5c2175dc6533215a32fe
Adam Orłowski <orlowski.adam.99@gmail.com>
2023-09-09 17:51:22 +0200

    [AC-46] Removed tournament interfaces that were named wrongly

--- src/main/java/xyz/aimcup/tournament/service/tournament/ITeamBasedTournamentService.java
+++
@@ -1,8 +0,0 @@
-package xyz.aimcup.tournament.service.tournament;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface ITeamBasedTournamentService extends ITournamentService {
-
-}
--- src/main/java/xyz/aimcup/tournament/service/tournament/ITournamentBaseService.java
+++
@@ -1,8 +0,0 @@
-package xyz.aimcup.tournament.service.tournament;
-
-import xyz.aimcup.generated.model.TournamentCreateRequest;
-import xyz.aimcup.tournament.data.entity.tournament.Tournament;
-
-public interface ITournamentBaseService {
-    Tournament createTournament(TournamentCreateRequest tournamentCreateRequest);
-}
