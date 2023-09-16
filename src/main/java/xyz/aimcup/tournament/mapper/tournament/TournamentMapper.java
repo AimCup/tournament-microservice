ff518936116f6ca7be10093e700d16208169259f
Maciej Słowiak <slowiakmaciej@gmail.com>
2023-09-16 16:56:28 +0200

    [AC-49] Change naming in openApi files

--- src/main/java/xyz/aimcup/tournament/mapper/tournament/TournamentMapper.java
+++ src/main/java/xyz/aimcup/tournament/mapper/tournament/TournamentMapper.java
@@ -2,12 +2,26 @@ package xyz.aimcup.tournament.mapper.tournament;
 
 import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+import xyz.aimcup.generated.model.CreateTournamentRequest;
 import xyz.aimcup.generated.model.TournamentResponseDto;
+import xyz.aimcup.tournament.data.entity.tournament.InternationalTournament;
+import xyz.aimcup.tournament.data.entity.tournament.PlayerBasedTournament;
+import xyz.aimcup.tournament.data.entity.tournament.TeamBasedTournament;
 import xyz.aimcup.tournament.data.entity.tournament.Tournament;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface TournamentMapper {
-    TournamentResponseDto toTournamentResponseDto(Tournament tournament);
 
-    List<TournamentResponseDto> toTournamentResponseDtos(List<Tournament> tournaments);
+    TournamentResponseDto toTournamentResponseDtoFrom(Tournament tournament);
+
+    List<TournamentResponseDto> toTournamentResponseDtoListFrom(List<Tournament> tournaments);
+
+    PlayerBasedTournament toPlayerBasedTournamentFrom(
+        CreateTournamentRequest createTournamentRequest);
+
+    InternationalTournament toInternationalTournamentFrom(
+        CreateTournamentRequest createTournamentRequest);
+
+    TeamBasedTournament toTeamBasedTournamentFrom(CreateTournamentRequest createTournamentRequest);
 }
--- src/main/java/xyz/aimcup/tournament/service/tournament/TournamentBaseService.java
+++ src/main/java/xyz/aimcup/tournament/service/tournament/TournamentBaseService.java
@@ -1,10 +1,9 @@
 package xyz.aimcup.tournament.service.tournament;
 
-import xyz.aimcup.generated.model.TournamentCreateRequest;
+import xyz.aimcup.generated.model.CreateTournamentRequest;
 import xyz.aimcup.tournament.data.entity.tournament.Tournament;
 
 public interface TournamentBaseService {
-    Tournament createTournament(TournamentCreateRequest tournamentCreateRequest);
-//    Tournament getTournamentById(UUID tournamentId);
-//    List<Tournament> getTournaments();
+
+    Tournament createTournament(CreateTournamentRequest createTournamentRequest);
 }
