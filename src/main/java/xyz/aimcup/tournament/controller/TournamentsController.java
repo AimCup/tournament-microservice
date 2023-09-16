575d006b16bf84a62af4add74c759f678447c94a
Maciej Słowiak <slowiakmaciej@gmail.com>
2023-09-16 16:56:28 +0200

    [AC-49] Change naming in openApi files

--- src/main/java/xyz/aimcup/tournament/controller/TournamentsController.java
+++ src/main/java/xyz/aimcup/tournament/controller/TournamentsController.java
@@ -8,8 +8,8 @@ import org.apache.commons.lang.NotImplementedException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.RestController;
-import xyz.aimcup.generated.TournamentApi;
-import xyz.aimcup.generated.model.TournamentCreateRequest;
+import xyz.aimcup.generated.TournamentsApi;
+import xyz.aimcup.generated.model.CreateTournamentRequest;
 import xyz.aimcup.generated.model.TournamentResponseDto;
 import xyz.aimcup.tournament.mapper.tournament.TournamentMapper;
 import xyz.aimcup.tournament.service.tournament.TournamentService;
@@ -17,7 +17,7 @@ import xyz.aimcup.tournament.service.tournament.TournamentServiceFacade;
 
 @RestController
 @RequiredArgsConstructor
-public class TournamentController implements TournamentApi {
+public class TournamentsController implements TournamentsApi {
 
     private final TournamentServiceFacade tournamentServiceFacade;
     private final TournamentService tournamentService;
@@ -25,23 +25,23 @@ public class TournamentController implements TournamentApi {
 
     @Secured("ROLE_ADMIN")
     @Override
-    public ResponseEntity<UUID> createTournament(TournamentCreateRequest tournamentCreateRequest) {
-        var tournament = tournamentServiceFacade.createTournament(tournamentCreateRequest);
+    public ResponseEntity<UUID> createTournament(CreateTournamentRequest createTournamentRequest) {
+        var tournament = tournamentServiceFacade.createTournament(createTournamentRequest);
         return ResponseEntity.created(URI.create("/tournament/" + tournament.getId()))
             .body(tournament.getId());
     }
 
     @Override
-    public ResponseEntity<TournamentResponseDto> getTournamentById(UUID tournamentId) {
+    public ResponseEntity<TournamentResponseDto> getTournament(UUID tournamentId) {
         var tournament = tournamentService.getTournamentById(tournamentId);
-        var tournamentResponseDto = tournamentMapper.toTournamentResponseDto(tournament);
+        var tournamentResponseDto = tournamentMapper.toTournamentResponseDtoFrom(tournament);
         return ResponseEntity.ok(tournamentResponseDto);
     }
 
     @Override
     public ResponseEntity<List<TournamentResponseDto>> getTournaments() {
         var tournaments = tournamentService.getTournaments();
-        var tournamentResponseDtos = tournamentMapper.toTournamentResponseDtos(tournaments);
+        var tournamentResponseDtos = tournamentMapper.toTournamentResponseDtoListFrom(tournaments);
         return ResponseEntity.ok(tournamentResponseDtos);
     }
 
