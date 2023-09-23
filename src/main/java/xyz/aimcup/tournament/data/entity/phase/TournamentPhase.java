1d0b3a99c1e759f832b52312e446d72d2ebe1c78
Maciej Słowiak <slowiakmaciej@gmail.com>
2023-09-23 16:34:44 +0200

    [AC-49] Change structure of Tournament Phases

--- src/main/java/xyz/aimcup/tournament/data/entity/phase/TournamentPhase.java
+++ src/main/java/xyz/aimcup/tournament/data/entity/phase/TournamentPhase.java
@@ -1,5 +1,30 @@
 package xyz.aimcup.tournament.data.entity.phase;
 
-public interface TournamentPhase {
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
+@Getter
+@MappedSuperclass
+@SuperBuilder
+@NoArgsConstructor
+public abstract class TournamentPhase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    public Boolean isPhaseActiveNow() {
+        return LocalDateTime.now().isAfter(startTime) && LocalDateTime.now().isBefore(endTime);
+    }
 }
