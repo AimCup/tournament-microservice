d4b5afb383168f7648a40655cdb4f7548188563b
Maciej Słowiak <slowiakmaciej@gmail.com>
2023-11-25 17:06:21 +0100

    [AC-50] Change keycloak configuration

--- src/main/java/xyz/aimcup/tournament/configuration/SecurityConfiguration.java
+++ src/main/java/xyz/aimcup/tournament/configuration/SecurityConfiguration.java
@@ -1,18 +1,31 @@
 package xyz.aimcup.tournament.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@Slf4j
 public class SecurityConfiguration {
+
+    private final ClientRegistrationRepository clientRegistrationRepository;
+
+    public SecurityConfiguration(ClientRegistrationRepository clientRegistrationRepository) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
+
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+        ClientRegistration clientRegistration = this.clientRegistrationRepository.findByRegistrationId("aimcup");
+        log.info("clientRegistration: {}", clientRegistration);
         httpSecurity
                 .oauth2Client(Customizer.withDefaults())
                 .oauth2Login(Customizer.withDefaults());
